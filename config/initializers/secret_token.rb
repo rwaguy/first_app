3d20ff4968dac938d6a17e2e36b18a923fe3ec80
sr <st1d@wildmail.com>
2013-03-29 15:40:11 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '41df54934e0ba8baa756c86453956c426f7ef9f4f5a1a0d706a05f3c421578d58736bfef3e234d0c3478714832eadf75806f925d1b821dcbed4a2e534dd65f01'
